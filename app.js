30534219ea9a1e765721c0b440a2c1dcb57a0290
Xinyuan Zhang <xiz016@ucsd.edu>
2016-01-27 04:22:49 +0000

    node.js lab

--- app.js
+++ app.js
@@ -11,8 +11,10 @@ var handlebars = require('express3-handlebars')
 var index = require('./routes/index');
 // Example route
 // var user = require('./routes/user');
+var project = require('./routes/project');
 
 var app = express();
+
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -38,6 +40,7 @@ if ('development' == app.get('env')) {
 app.get('/', index.view);
 // Example route
 // app.get('/users', user.list);
+app.get('/project/:name', project.viewProject);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
